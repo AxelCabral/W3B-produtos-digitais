f31a7be643673402ff0e7cd3af3841f530274775
Axel Cabral <axel.cabral0102@gmail.com>
2025-04-15 01:02:28 -0300

    v0.9 dev

--- src/server.js
+++ src/server.js
@@ -60,6 +60,9 @@ function verifyKey(key){
   return contents[key] || null;
 }
 
-
 // Inicializa o servidor
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, '../public', '404.html'));
+});
